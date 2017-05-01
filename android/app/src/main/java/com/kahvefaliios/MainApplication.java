2e2adddfa798177c0d2b3a7fe53098e244a66746
Bora Unlu <boraunlu@Boras-MacBook-Pro.local>
2017-05-01 09:23:07 +0300

    ilk

---
+++ android/app/src/main/java/com/kahvefaliios/MainApplication.java
@@ -0,0 +1,50 @@
+package com.kahvefaliios;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNFetchBlobPackage(),
+            new RNSoundPackage(),
+            new ReactVideoPackage(),
+            new RCTCameraPackage(),
+            new FBSDKPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
